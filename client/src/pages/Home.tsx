120e3e78e51ce75c0cebcf0792fab10d381916dc
llOrtegall <IvanOrtega_97@hotmail.com>
2025-01-30 15:07:50 -0500

    Fetch and display data from API in Home component

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,13 +1,31 @@
+import { URL_API_DATA } from '../utils/contanst'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
 
 function Home() {
+  const [test, setTest] = useState('')
 
-    return (
-        <section className=''>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-        </section>
-    )
+  useEffect(() => {
+    axios.get(`${URL_API_DATA}`)
+      .then(response => {
+        console.log(response)
+        setTest(response.data)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [])
+
+  return (
+    <section className=''>
+      <h1 className='text-3xl font-bold underline'>
+        Saludo desde api de pqrs:
+      </h1>
+      <span>
+        {test}
+      </span>
+    </section>
+  )
 }
 
 export default Home
