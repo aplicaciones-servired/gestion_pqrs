bd068d935e22145103e61651fed33778908f7dcb
Servired - Multired <aplicaciones@grupomultired.com.co>
2025-05-08 11:22:36 -0500

    Update LogOut.ts

--- client/src/services/LogOut.ts
+++ client/src/services/LogOut.ts
@@ -1,15 +1,10 @@
 import { LOGIN_URL } from '../utils/contanst';
-import axios from 'axios';
+import axios from 'axios'
 
-export const LogoutAndDeleteToken = async () => {
-  const token = document.cookie;
-
-  try {
-    const res = await axios.post(`${LOGIN_URL}/logout`, { token });
-    if (res.status === 200)
-      return true;
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
-};
+export const LogoutAndDeleteToken = () => {
+  axios.get(`${LOGIN_URL}/logout`)
+    .then(res => {
+      console.log(res)
+    })
+    .catch(err => console.log(err))
+}
